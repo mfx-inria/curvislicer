813d81b1c0bb9b64032cbdd800d04de4e8f788bf
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2020-03-20 14:51:01 +0100

    updated to latest TetWild

--- toTetmesh.bat
+++ toTetmesh.bat
@@ -3,7 +3,7 @@ set a=%1
 set a=%a:/=\%
 
 if not exist %a%.msh (
-  .\tools\tetwild-windows\TetWild.exe --save-mid-result 2 %a%.stl -e 0.025 --targeted-num-v 1000 --is-laplacian
+  .\tools\tetwild-windows\TetWild.exe --save-mid-result 2 %a%.stl -e 0.001 --targeted-num-v 1000 --is-laplacian
   move "%a%__mid2.000000.msh" "%a%.msh"
 )
 
