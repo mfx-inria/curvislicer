fa70899627524289595148991b8d8298aa0fc1d7
JuDePom <jimmy.etienne@aliceadsl.fr>
2020-12-17 14:19:50 +0100

    Revert "Fix: ThresholdArg, sin instead of cos"
    This reverts commit e8b8151bd5c32ad84a874584c383b014f07101bd.

--- src/main.cpp
+++ src/main.cpp
@@ -976,7 +976,7 @@ int main(int argc, char **argv)
 
     normal_threshold = thresholdArg.isSet()
                        ? thresholdArg.getValue()
-                       : sin(max_theta * M_PI / 180);
+                       : cos(max_theta * M_PI / 180);
     fabricable_emptiness = fabemptyArg.getValue();
 
     if (  timeArg.isSet()) compute_time = (float)timeArg.getValue();
